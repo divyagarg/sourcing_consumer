6e1b325dfeff0cc5cc60b05ea67fa823602573c8
Ansal007 <anas@askme.in>
2016-03-20 11:18:14 +0530

    removing print and printing traceback

--- orderengine.py
+++ orderengine.py
@@ -3,6 +3,7 @@ import datetime
 from config import KAFKA_TOPIC, KAFKA_GROUP, KAFKA_HOSTS, APP_NAME, FINANCE_SERVICE_TOKEN, \
     FINANCE_SERVICE_CREATE_ORDERS_URL
 import requests
+import sys, traceback
 __author__ = 'Ansal007'
 import logging
 Logger = logging.getLogger(APP_NAME)
@@ -30,9 +31,9 @@ class OrderEngineConsumer(threading.Thread):
                 timetaken = endtime - starttime
                 total_time = timetaken.seconds*1000000 + timetaken.microseconds
                 Logger.info('Total time taken to consume order %s'%total_time)
-                print message
 
         except Exception as ex:
+            traceback.print_exc()
             Logger.error('Exception while consuming messsege from partition', exc_info=True)
 
     def publish_message_to_finance_service(self, message):
@@ -50,6 +51,7 @@ class OrderEngineConsumer(threading.Thread):
             res = requests.post(url=FINANCE_SERVICE_CREATE_ORDERS_URL, data=json.dumps(data))
             Logger.info('Order posted for orderid {%s} with status code {%s} and status text{%s}'%(message.key, res.status_code, res.text))
         except Exception as e:
+            traceback.print_exc()
             Logger.error('Exception while consuming data', exc_info=True)
 
 
